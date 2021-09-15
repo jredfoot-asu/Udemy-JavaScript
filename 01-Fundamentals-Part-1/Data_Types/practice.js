23a21c6704162409aaac5de7ddaf5123b7a91252
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-09-15 15:36:21 -0400

    Completed the Data Types section and the practice for that section.

---
+++ 01-Fundamentals-Part-1/Data_Types/practice.js
@@ -0,0 +1,18 @@
+let country = "United States";
+let continent = "North America";
+let population = 350;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log("------------");
+
+let isIsland = country;
+console.log(isIsland);
+isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
--- 01-Fundamentals-Part-1/Data_Types/script.js
+++ 01-Fundamentals-Part-1/Data_Types/script.js
@@ -1,8 +1,19 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-console.log('Jonas');
-console.log(23);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
 
-let firstName = "Jonas";
-console.log(firstName);
+// This is an example of Dynamic Typing. We are changing the value of the variable javascriptIsFun.
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+// creating a variable that's undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);

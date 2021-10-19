7d76b4d6a2409a0da69ec417c5b902c9d37518fa
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-10-19 10:11:38 -0400

    Adding the coding challenge code for challenge 3

---
+++ 01-Fundamentals-Part-1/coding_challenge_3/script.js
@@ -0,0 +1,23 @@
+// const scoreDolphins = (96 + 108 + 89) /3;
+// const scoreKoalas = (88 + 91 + 110) /3;
+// console.log(scoreDolphins, scoreKoalas);
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas){
+//   console.log('Both win the trophy 🏆');
+// }
+
+const scoreDolphins = (97 + 112 + 80) /3;
+const scoreKoalas = (109 + 95 + 50) /3;
+console.log(scoreDolphins, scoreKoalas);
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins win the trophy 🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100){
+  console.log('Both win the trophy 🏆');
+} else {
+  console.log('No one wins the trophy 😢');
+}

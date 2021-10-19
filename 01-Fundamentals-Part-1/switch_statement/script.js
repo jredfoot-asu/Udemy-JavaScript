2d8a3c1fd06e254ee966f9308c3cfc27986d7fce
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-10-19 10:25:27 -0400

    Adding a section on the switch statement

---
+++ 01-Fundamentals-Part-1/switch_statement/script.js
@@ -0,0 +1,28 @@
+const day = 'monday';
+
+switch (day) {
+  case 'monday':
+    console.log('Plan course structure');
+    console.log('Go to coding meeting');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+    console.log('Nothing at this time');
+    break;
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+    console.log('Nothing at this time');
+    break;
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day');
+}

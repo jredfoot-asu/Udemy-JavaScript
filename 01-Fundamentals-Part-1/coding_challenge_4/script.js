0c048013b9e1c458d0c199e6b9311bb18e8443d2
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-10-26 11:11:36 -0400

    Playing around with different studio code settings.

--- 01-Fundamentals-Part-1/coding_challenge_4/script.js
+++ 01-Fundamentals-Part-1/coding_challenge_4/script.js
@@ -1,3 +1,5 @@
 const bill = 430;
-const tip = bill <= 300 && bill >= 50 ? bill * .15 : bill *.2;
-console.log(`The bill was ${bill}, the tipe was ${tip}, and the total value ${bill + tip}`);
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tipe was ${tip}, and the total value ${bill + tip}`
+);

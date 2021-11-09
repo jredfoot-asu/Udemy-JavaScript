b0c79cb1f445a5805a13e30faa1157c6516811c2
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-11-09 10:34:27 -0500

    Adding the code that demonstrates scopes and how they work.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    let output = `You are ${firstName}, born in ${birthYear}.`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // the current scope is the one that is used. JavaScript stops looking after the variable is found.
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+      output = 'NEW OUTPUT';
+    }
+    // can not access because outside scope
+    // console.log(str);
+    // var allows to traverse down.
+    // console.log(millenial);
+    // functions are allso constrained to their scope.
+    // console.log(add);
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jason';
+calcAge(1983);
+// can't call variables that are in functions
+// console.log(age);
+// printAge();

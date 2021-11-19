ac77e73b9da138b1d9c0a9d4dd2390cc2015458c
Jason Redfoot <jredfoot@Jasons-MacBook-Pro.local>
2021-11-19 10:19:27 -0500

    Adding the section of script where Primitives vs Objects are explained.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -33,3 +33,40 @@ calcAge(1983);
 // can't call variables that are in functions
 // console.log(age);
 // printAge();
+
+// Primatives vs Objects
+// Primative Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage: ', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+// Object.assign only creates a a shadow copy
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+// The array is changed in both because the family object is in a deeper object. This requires an external library. This will be shown in a latter video.
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
